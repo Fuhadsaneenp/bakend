7b530c2940dac15500829d09e5d96c5609ab0a51
fuhad-saneen <saneensane007@gmail.com>
2026-07-14 15:25:31 +0530

    chore: add startup crash logging to server.js

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-// Dynamic import of the compiled entry point for Hostinger compatibility
-import("./dist/src/server.js");
+import fs from "fs";
+
+process.on("unhandledRejection", (reason) => {
+  const msg = `Unhandled Rejection: ${reason}\n${reason?.stack || ""}\n`;
+  fs.writeFileSync("stderr.log", msg, { flag: "a" });
+});
+
+process.on("uncaughtException", (error) => {
+  const msg = `Uncaught Exception: ${error}\n${error?.stack || ""}\n`;
+  fs.writeFileSync("stderr.log", msg, { flag: "a" });
+});
+
+try {
+  await import("./dist/src/server.js");
+} catch (error) {
+  const msg = `Import Error: ${error}\n${error?.stack || ""}\n`;
+  fs.writeFileSync("stderr.log", msg, { flag: "a" });
+  throw error;
+}
